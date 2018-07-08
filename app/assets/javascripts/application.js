9bef8e47f386aed57e9c2a73d284cb41dbeaba61
Shintaro Konno <merfolk@nifty.com>
2018-07-08 06:04:49 +0000

    test]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery/dist/jquery.js
-//= require jquery/dist/jquery-migrate.min.js
-//= require bootstrap/dist/js/bootstrap.min.js
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
